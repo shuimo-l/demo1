e9de48b37e9c8e33c8d6240ddc442109163283c2
ran <wu@qq.com>
2018-11-09 10:00:15 +0800

    modify application

--- src/main/java/com/demo/demo1/Demo1Application.java
+++ src/main/java/com/demo/demo1/Demo1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo1Application {
-
+	//加行注释
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
